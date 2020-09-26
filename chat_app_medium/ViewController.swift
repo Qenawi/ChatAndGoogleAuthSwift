5402188b1bfd2270890a54c85c0930d51ca9c2e9
Qenawi <aldmanmero@yahoo.com>
2020-09-26 16:10:26 +0200

    Initial Commit

---
+++ chat_app_medium/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chat_app_medium
+//
+//  Created by Qenawi on 9/26/20.
+//  Copyright © 2020 goplus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
